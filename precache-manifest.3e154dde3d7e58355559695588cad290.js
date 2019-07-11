9db06c28a9dbb99c9d4a460d360051449e5c4e94
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-11 08:22:26 +0200

    Updates

---
+++ precache-manifest.3e154dde3d7e58355559695588cad290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0090b7037f746c508403d3106cc5139",
+    "url": "/lyrycsapp/index.html"
+  },
+  {
+    "revision": "e8cb7c7bb039e4361409",
+    "url": "/lyrycsapp/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "585b0f690c2fc72fffdd",
+    "url": "/lyrycsapp/static/js/2.1bcf467a.chunk.js"
+  },
+  {
+    "revision": "e8cb7c7bb039e4361409",
+    "url": "/lyrycsapp/static/js/main.e55557ba.chunk.js"
+  },
+  {
+    "revision": "d6eea2a365c78daaaad4",
+    "url": "/lyrycsapp/static/js/runtime~main.feb6b863.js"
+  }
+]);
